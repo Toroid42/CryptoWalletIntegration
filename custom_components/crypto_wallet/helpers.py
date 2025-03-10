7eb9607357cdcc045ae46ad9df8e697962776bdd
Toroid42 <toroid@42>
2025-03-10 21:14:00 +0100

    feat: make scan_interval configurable

--- custom_components/crypto_wallet/helpers.py
+++ custom_components/crypto_wallet/helpers.py
@@ -140,6 +140,7 @@ class CryptoWalletTotalSensor(SensorEntity):
             "vs_currencies": f"{selected_currency}",
         }
         if access_token:
+            _LOGGER.debug(f"Using API-Key")
             headers = {"x-cg-demo-api-key": f"{access_token}"}
         else:
             headers = None
